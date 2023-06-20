bb072c0605e0481d89f575d78e067bcf71a253e9
Gleb <glebpachan@gmail.com>
2023-06-20 13:53:45 +0000

    Added better Ytsaurus validation & affinity, deprecated EnableAntiAffinity flag
    Pull Request resolved: #17

--- api/v1/ytsaurus_webhook.go
+++ api/v1/ytsaurus_webhook.go
@@ -18,8 +18,11 @@ package v1
 
 import (
 	"fmt"
+
 	"github.com/ytsaurus/yt-k8s-operator/pkg/consts"
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
@@ -47,9 +50,23 @@ var _ webhook.Defaulter = &Ytsaurus{}
 func (r *Ytsaurus) Default() {
 	ytsauruslog.Info("default", "name", r.Name)
 
-	if r.Spec.PrimaryMasters.EnableAntiAffinity == nil {
-		r.Spec.PrimaryMasters.EnableAntiAffinity = new(bool)
-		*r.Spec.PrimaryMasters.EnableAntiAffinity = true
+	// Set anti affinity for masters
+	if r.Spec.PrimaryMasters.Affinity == nil {
+		r.Spec.PrimaryMasters.Affinity = &corev1.Affinity{}
+	}
+	if r.Spec.PrimaryMasters.Affinity.PodAntiAffinity == nil {
+		r.Spec.PrimaryMasters.Affinity.PodAntiAffinity = &corev1.PodAntiAffinity{
+			RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
+				{
+					LabelSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							consts.YTComponentLabelName: fmt.Sprintf("%s-%s", r.Name, "yt-master"),
+						},
+					},
+					TopologyKey: "kubernetes.io/hostname",
+				},
+			},
+		}
 	}
 }
 
@@ -58,64 +75,223 @@ func (r *Ytsaurus) Default() {
 
 var _ webhook.Validator = &Ytsaurus{}
 
-func (r *Ytsaurus) validateProxies(spec YtsaurusSpec) field.ErrorList {
-	var allErrs field.ErrorList
+//////////////////////////////////////////////////
+
+func (r *Ytsaurus) validateDiscovery() field.ErrorList {
+	var allErrors field.ErrorList
+
+	allErrors = append(allErrors, r.validateInstanceSpec(r.Spec.Discovery.InstanceSpec, field.NewPath("spec").Child("discovery"))...)
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validatePrimaryMasters() field.ErrorList {
+	var allErrors field.ErrorList
+
+	allErrors = append(allErrors, r.validateInstanceSpec(r.Spec.PrimaryMasters.InstanceSpec, field.NewPath("spec").Child("primaryMasters"))...)
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateSecondaryMasters() field.ErrorList {
+	var allErrors field.ErrorList
+
+	for i, sm := range r.Spec.SecondaryMasters {
+		path := field.NewPath("spec").Child("secondaryMasters").Index(i)
+		allErrors = append(allErrors, r.validateInstanceSpec(sm.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateHTTPProxies() field.ErrorList {
+	var allErrors field.ErrorList
 
 	httpRoles := make(map[string]bool)
-	rpcRoles := make(map[string]bool)
 	hasDefaultHTTPProxy := false
-	for _, hp := range spec.HTTPProxies {
+	for i, hp := range r.Spec.HTTPProxies {
+		path := field.NewPath("spec").Child("httpProxies").Index(i)
 		if _, exists := httpRoles[hp.Role]; exists {
-			allErrs = append(allErrs, field.Duplicate(field.NewPath("spec").Child("httpProxies").Child("role"), hp.Role))
+			allErrors = append(allErrors, field.Duplicate(path.Child("role"), hp.Role))
 		}
 		if hp.Role == consts.DefaultHTTPProxyRole {
 			hasDefaultHTTPProxy = true
 		}
 		httpRoles[hp.Role] = true
+
+		allErrors = append(allErrors, r.validateInstanceSpec(hp.InstanceSpec, path)...)
 	}
 
 	if !hasDefaultHTTPProxy {
-		allErrs = append(allErrs, field.Required(
-			field.NewPath("spec").Child("httpProxies").Child("role"),
+		allErrors = append(allErrors, field.Required(
+			field.NewPath("spec").Child("httpProxies"),
 			fmt.Sprintf("HTTP proxy with `%s` role should exist", consts.DefaultHTTPProxyRole)))
 	}
 
-	for _, rp := range spec.RPCProxies {
-		if _, exists := rpcRoles[rp.Role]; exists {
-			allErrs = append(allErrs, field.Duplicate(field.NewPath("spec").Child("rpcProxies").Child("role"), rp.Role))
-		}
-		rpcRoles[rp.Role] = true
-	}
-
-	return allErrs
+	return allErrors
 }
 
-func (r *Ytsaurus) validateYtsaurus() error {
-	var allErrs field.ErrorList
+func (r *Ytsaurus) validateRPCProxies() field.ErrorList {
+	var allErrors field.ErrorList
 
-	allErrs = append(allErrs, r.validateProxies(r.Spec)...)
-	if len(allErrs) == 0 {
+	rpcRoles := make(map[string]bool)
+	for i, rp := range r.Spec.RPCProxies {
+		path := field.NewPath("spec").Child("rpcProxies").Index(i)
+		if _, exists := rpcRoles[rp.Role]; exists {
+			allErrors = append(allErrors, field.Duplicate(path.Child("role"), rp.Role))
+		}
+		rpcRoles[rp.Role] = true
+
+		allErrors = append(allErrors, r.validateInstanceSpec(rp.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateDataNodes() field.ErrorList {
+	var allErrors field.ErrorList
+
+	for i, dn := range r.Spec.DataNodes {
+		path := field.NewPath("spec").Child("dataNodes").Index(i)
+		allErrors = append(allErrors, r.validateInstanceSpec(dn.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateExecNodes() field.ErrorList {
+	var allErrors field.ErrorList
+
+	for i, en := range r.Spec.ExecNodes {
+		path := field.NewPath("spec").Child("execNodes").Index(i)
+		allErrors = append(allErrors, r.validateInstanceSpec(en.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateSchedulers() field.ErrorList {
+	var allErrors field.ErrorList
+
+	if r.Spec.Schedulers != nil {
+		path := field.NewPath("spec").Child("schedulers")
+		allErrors = append(allErrors, r.validateInstanceSpec(r.Spec.Schedulers.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateControllerAgents() field.ErrorList {
+	var allErrors field.ErrorList
+
+	if r.Spec.ControllerAgents != nil {
+		path := field.NewPath("spec").Child("controllerAgents")
+		allErrors = append(allErrors, r.validateInstanceSpec(r.Spec.ControllerAgents.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateTabletNodes() field.ErrorList {
+	var allErrors field.ErrorList
+
+	for i, tn := range r.Spec.TabletNodes {
+		path := field.NewPath("spec").Child("tabletNodes").Index(i)
+		allErrors = append(allErrors, r.validateInstanceSpec(tn.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateChyt() field.ErrorList {
+	var allErrors field.ErrorList
+	return allErrors
+}
+
+func (r *Ytsaurus) validateQueryTrackers() field.ErrorList {
+	var allErrors field.ErrorList
+
+	if r.Spec.QueryTrackers != nil {
+		path := field.NewPath("spec").Child("queryTrackers")
+		allErrors = append(allErrors, r.validateInstanceSpec(r.Spec.QueryTrackers.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateSpyt() field.ErrorList {
+	var allErrors field.ErrorList
+	return allErrors
+}
+
+func (r *Ytsaurus) validateYQLAgents() field.ErrorList {
+	var allErrors field.ErrorList
+
+	if r.Spec.YQLAgents != nil {
+		path := field.NewPath("spec").Child("YQLAgents")
+		allErrors = append(allErrors, r.validateInstanceSpec(r.Spec.YQLAgents.InstanceSpec, path)...)
+	}
+
+	return allErrors
+}
+
+//////////////////////////////////////////////////
+
+func (r *Ytsaurus) validateInstanceSpec(instanceSpec InstanceSpec, path *field.Path) field.ErrorList {
+	var allErrors field.ErrorList
+
+	if instanceSpec.EnableAntiAffinity != nil {
+		allErrors = append(allErrors, field.Invalid(path.Child("EnableAntiAffinity"), instanceSpec.EnableAntiAffinity, "EnableAntiAffinity is deprecated, use Affinity instead"))
+	}
+
+	return allErrors
+}
+
+func (r *Ytsaurus) validateYtsaurus() field.ErrorList {
+	var allErrors field.ErrorList
+
+	allErrors = append(allErrors, r.validateDiscovery()...)
+	allErrors = append(allErrors, r.validatePrimaryMasters()...)
+	allErrors = append(allErrors, r.validateSecondaryMasters()...)
+	allErrors = append(allErrors, r.validateHTTPProxies()...)
+	allErrors = append(allErrors, r.validateRPCProxies()...)
+	allErrors = append(allErrors, r.validateDataNodes()...)
+	allErrors = append(allErrors, r.validateExecNodes()...)
+	allErrors = append(allErrors, r.validateSchedulers()...)
+	allErrors = append(allErrors, r.validateControllerAgents()...)
+	allErrors = append(allErrors, r.validateTabletNodes()...)
+	allErrors = append(allErrors, r.validateChyt()...)
+	allErrors = append(allErrors, r.validateQueryTrackers()...)
+	allErrors = append(allErrors, r.validateSpyt()...)
+	allErrors = append(allErrors, r.validateYQLAgents()...)
+
+	return allErrors
+}
+
+func (r *Ytsaurus) evaluateYtsaurusValidation() error {
+	allErrors := r.validateYtsaurus()
+	if len(allErrors) == 0 {
 		return nil
 	}
 
 	return apierrors.NewInvalid(
 		schema.GroupKind{Group: "cluster.ytsaurus.tech", Kind: "Ytsaurus"},
 		r.Name,
-		allErrs)
+		allErrors)
 }
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Ytsaurus) ValidateCreate() error {
 	ytsauruslog.Info("validate create", "name", r.Name)
 
-	return r.validateYtsaurus()
+	return r.evaluateYtsaurusValidation()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Ytsaurus) ValidateUpdate(old runtime.Object) error {
 	ytsauruslog.Info("validate update", "name", r.Name)
 
-	return r.validateYtsaurus()
+	return r.evaluateYtsaurusValidation()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
--- api/v1/ytsaurus_webhooks_test.go
+++ api/v1/ytsaurus_webhooks_test.go
@@ -3,15 +3,15 @@ package v1
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Test for Ytsaurus webhooks", func() {
 	const namespace string = "default"
 
 	Context("When setting up the test environment", func() {
-		It("Should not accept Ytsaurus spec", func() {
-			By("Creating a Ytsaurus resource without `default` http proxy role")
-
+		It("Should not accept a Ytsaurus resource without `default` http proxy role", func() {
 			ytsaurus := CreateBaseYtsaurusResource(namespace)
 
 			ytsaurus.Spec.HTTPProxies = []HTTPProxiesSpec{
@@ -24,10 +24,10 @@ var _ = Describe("Test for Ytsaurus webhooks", func() {
 			}
 
 			Expect(k8sClient.Create(ctx, ytsaurus)).Should(MatchError(ContainSubstring("HTTP proxy with `default` role should exist")))
+		})
 
-			By("Creating a Ytsaurus resource with the same RPC proxies roles")
-
-			ytsaurus = CreateBaseYtsaurusResource(namespace)
+		It("Should not accept a Ytsaurus resource with the same RPC proxies roles", func() {
+			ytsaurus := CreateBaseYtsaurusResource(namespace)
 
 			ytsaurus.Spec.RPCProxies = []RPCProxiesSpec{
 				{
@@ -42,11 +42,11 @@ var _ = Describe("Test for Ytsaurus webhooks", func() {
 				},
 			}
 
-			Expect(k8sClient.Create(ctx, ytsaurus)).Should(MatchError(ContainSubstring("spec.rpcProxies.role: Duplicate value: \"default\"")))
+			Expect(k8sClient.Create(ctx, ytsaurus)).Should(MatchError(ContainSubstring("spec.rpcProxies[1].role: Duplicate value: \"default\"")))
+		})
 
-			By("Creating a Ytsaurus resource with the same HTTP proxies roles")
-
-			ytsaurus = CreateBaseYtsaurusResource(namespace)
+		It("Should not accept a Ytsaurus resource with the same HTTP proxies roles", func() {
+			ytsaurus := CreateBaseYtsaurusResource(namespace)
 
 			ytsaurus.Spec.HTTPProxies = []HTTPProxiesSpec{
 				{
@@ -61,8 +61,53 @@ var _ = Describe("Test for Ytsaurus webhooks", func() {
 				},
 			}
 
-			Expect(k8sClient.Create(ctx, ytsaurus)).Should(MatchError(ContainSubstring("spec.httpProxies.role: Duplicate value: \"default\"")))
-
+			Expect(k8sClient.Create(ctx, ytsaurus)).Should(MatchError(ContainSubstring("spec.httpProxies[1].role: Duplicate value: \"default\"")))
 		})
+
+		It("Should not accept a Ytsaurus resource with EnableAntiAffinity flag set in different spec fields", func() {
+			ytsaurus := CreateBaseYtsaurusResource(namespace)
+
+			trueEnableAntiAffinity := true
+			falseEnableAntiAffinity := false
+
+			ytsaurus.Spec.DataNodes = []DataNodesSpec{
+				{
+					InstanceSpec: InstanceSpec{
+						EnableAntiAffinity: &trueEnableAntiAffinity,
+					},
+				},
+				{
+					InstanceSpec: InstanceSpec{
+						EnableAntiAffinity: &falseEnableAntiAffinity,
+					},
+				},
+			}
+			ytsaurus.Spec.ControllerAgents = &ControllerAgentsSpec{
+				InstanceSpec: InstanceSpec{
+					EnableAntiAffinity: &trueEnableAntiAffinity,
+				},
+			}
+			ytsaurus.Spec.PrimaryMasters = MastersSpec{
+				InstanceSpec: InstanceSpec{
+					EnableAntiAffinity: &trueEnableAntiAffinity,
+				},
+			}
+
+			err := k8sClient.Create(ctx, ytsaurus)
+			statusErr, isStatus := err.(*apierrors.StatusError)
+			Expect(isStatus).To(BeTrue())
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("EnableAntiAffinity is deprecated, use Affinity instead"))
+
+			errorDetails := statusErr.ErrStatus.Details
+			Expect(len(errorDetails.Causes)).To(Equal(4))
+			for _, cause := range errorDetails.Causes {
+				Expect(cause.Type).To(Equal(metav1.CauseTypeFieldValueInvalid))
+				Expect(cause.Message).To(ContainSubstring("EnableAntiAffinity is deprecated, use Affinity instead"))
+			}
+
+			Expect(k8sClient.Create(ctx, ytsaurus)).Should(MatchError(ContainSubstring("EnableAntiAffinity is deprecated, use Affinity instead")))
+		})
+
 	})
 })
