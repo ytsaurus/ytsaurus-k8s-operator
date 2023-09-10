9671d86ea9c2244eb7ad7c357b3eb05a5e07a93e
Maxim Akhmedov <max42@ytsaurus.tech>
2023-09-10 23:00:36 +0200

    Fix ENABLE_WEBHOOKS behavior

--- main.go
+++ main.go
@@ -145,9 +145,11 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Chyt")
 		os.Exit(1)
 	}
-	if err = (&clusterv1.Chyt{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Chyt")
-		os.Exit(1)
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&clusterv1.Chyt{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Chyt")
+			os.Exit(1)
+		}
 	}
 	//+kubebuilder:scaffold:builder
 
