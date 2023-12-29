6907d9e58b7550a0a8604d73b38708bc4f580c84
Kirill Sibirev <sibirev@nebius.com>
2023-12-29 17:44:47 +0100

    WIP Sync impl

--- controllers/remotedatanodes_sync.go
+++ controllers/remotedatanodes_sync.go
@@ -13,30 +13,12 @@ import (
 	"github.com/ytsaurus/yt-k8s-operator/pkg/ytconfig"
 )
 
-type remoteMaster struct {
-	name string
-}
-
-func NewRemoteMaster(name string) *remoteMaster {
-	return &remoteMaster{name: name}
-}
-
-func (m *remoteMaster) GetName() string {
-	return m.name
-}
-func (m *remoteMaster) Status(_ context.Context) components.ComponentStatus {
-	return components.ComponentStatus{
-		SyncStatus: components.SyncStatusReady,
-		Message:    "Remote master status assumed ready",
-	}
-}
-
 func (r *RemoteDataNodesReconciler) Sync(ctx context.Context, resource *ytv1.RemoteDataNodes, remoteYtsaurus *ytv1.RemoteYtsaurus) (ctrl.Result, error) {
-	logger := log.FromContext(ctx)
+	logger := log.FromContext(ctx).WithValues("component", "remotedatanodes")
 
 	nodes := apiproxy.NewRemoteDataNodes(resource, r.Client, r.Recorder, r.Scheme)
 
-	_, err := ytconfig.NewNodeGenerator(
+	cfgen, err := ytconfig.NewNodeGenerator(
 		types.NamespacedName{Name: resource.Name, Namespace: resource.Namespace},
 		getClusterDomain(nodes.APIProxy().Client()),
 		&resource.Spec.ConfigurationSpec,
@@ -48,44 +30,36 @@ func (r *RemoteDataNodesReconciler) Sync(ctx context.Context, resource *ytv1.Rem
 		return ctrl.Result{}, err
 	}
 
-	//component := components.NewDataNodeConfigured(
-	//	cfgen,
-	//	components.NewRemoteYtsaurusStateManager(),
-	//	&nodes.GetResource().ObjectMeta,
-	//	nodes.APIProxy(),
-	//	&resource.Spec.ConfigurationSpec,
-	//	NewRemoteMaster(remoteYtsaurus.Name+" remote master"),
-	//	resource.Spec.DataNodesSpec,
-	//)
-	//
-	//if nodes.GetResource().Status.DeployStatus == ytv1.RemoteDataNodesStatusStarted {
-	//	return ctrl.Result{}, nil
-	//}
-	//
-	//err = component.Fetch(ctx)
-	//if err != nil {
-	//	logger.Error(err, "failed to fetch remote data nodes status for controller")
-	//	return ctrl.Result{Requeue: true}, err
-	//}
-	//
-	//status := component.Status(ctx)
-	//if status.SyncStatus == components.SyncStatusBlocked {
-	//	return ctrl.Result{RequeueAfter: time.Second * 10}, nil
-	//}
-	//if status.SyncStatus == components.SyncStatusReady {
-	//	logger.Info("Data nodes initialization finished")
-	//
-	//	err := nodes.SaveDeployStatus(ctx, ytv1.RemoteDataNodesStatusStarted)
-	//	return ctrl.Result{Requeue: true}, err
-	//}
-	//if err := component.Sync(ctx); err != nil {
-	//	logger.Error(err, "component sync failed", "component", "datanodes")
-	//	return ctrl.Result{Requeue: true}, err
-	//}
-	//if err := nodes.APIProxy().UpdateStatus(ctx); err != nil {
-	//	logger.Error(err, "update chyt status failed")
-	//	return ctrl.Result{Requeue: true}, err
-	//}
+	component := components.NewDataNodeRemote(
+		cfgen,
+		nodes,
+		resource.Spec.DataNodesSpec,
+	)
+
+	err = component.Fetch(ctx)
+	if err != nil {
+		logger.Error(err, "failed to fetch remote data nodes status for controller")
+		return ctrl.Result{Requeue: true}, err
+	}
+
+	status := component.Status(ctx)
+	if status.SyncStatus == components.SyncStatusReady {
+		logger.Info("component ready")
+		return ctrl.Result{}, nil
+	}
+
+	logger.Info("component sync")
+	if err = component.Sync(ctx); err != nil {
+		logger.Error(err, "component sync failed")
+		return ctrl.Result{Requeue: true}, err
+	}
+
+	// TODO: what status is this?
+	// TODO: CRD status is needed?
+	if err = nodes.APIProxy().UpdateStatus(ctx); err != nil {
+		logger.Error(err, "update status failed")
+		return ctrl.Result{Requeue: true}, err
+	}
 
 	return ctrl.Result{Requeue: true}, nil
 }
--- pkg/apiproxy/remotedatanodes.go
+++ pkg/apiproxy/remotedatanodes.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -35,10 +36,23 @@ func (n *RemoteDataNodes) GetResource() *ytv1.RemoteDataNodes {
 	return n.remoteDataNodes
 }
 
+func (n *RemoteDataNodes) GetObjectMeta() metav1.ObjectMeta {
+	return n.GetResource().ObjectMeta
+}
+
 func (n *RemoteDataNodes) SaveDeployStatus(ctx context.Context, status ytv1.RemoteDataNodesDeployStatus) error {
 	n.GetResource().Status.DeployStatus = status
 	if err := n.apiProxy.UpdateStatus(ctx); err != nil {
 		return fmt.Errorf("unable to update remote data nodes deploy status: %w", err)
 	}
 	return nil
+}
+
+func (n *RemoteDataNodes) GetConfigurationSpec() ytv1.ConfigurationSpec {
+	return n.GetResource().Spec.ConfigurationSpec
+}
+
+// TODO: how do we transit between statuses?
+func (n *RemoteDataNodes) IsUpdating() bool {
+	return n.GetResource().Status.DeployStatus == ytv1.RemoteDataNodesStatusUpdating
 }
