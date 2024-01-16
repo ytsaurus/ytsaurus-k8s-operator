5e74d0bb61fa4a0b33658315828a30ea25beb893
Konstantin Khlebnikov <koct9i@gmail.com>
2024-01-16 17:38:43 +0100

    Cleanup building spec for exec nodes

--- pkg/components/exec_node.go
+++ pkg/components/exec_node.go
@@ -18,10 +18,9 @@ import (
 
 type execNode struct {
 	localServerComponent
-	cfgen      *ytconfig.NodeGenerator
-	master     Component
-	sidecars   []string
-	privileged bool
+	cfgen        *ytconfig.NodeGenerator
+	master       Component
+	execNodeSpec *ytv1.ExecNodesSpec
 }
 
 func NewExecNode(
@@ -56,8 +55,7 @@ func NewExecNode(
 		localServerComponent: newLocalServerComponent(&l, ytsaurus, srv),
 		cfgen:                cfgen,
 		master:               master,
-		sidecars:             spec.Sidecars,
-		privileged:           spec.Privileged,
+		execNodeSpec:         &spec,
 	}
 }
 
@@ -67,6 +65,37 @@ func (n *execNode) IsUpdatable() bool {
 
 func (n *execNode) Fetch(ctx context.Context) error {
 	return resources.Fetch(ctx, n.server)
+}
+
+func (n *execNode) doBuild() error {
+	statefulSet := n.server.buildStatefulSet()
+	podSpec := &statefulSet.Spec.Template.Spec
+
+	setContainerPrivileged := func(ct *corev1.Container) {
+		if ct.SecurityContext == nil {
+			ct.SecurityContext = &corev1.SecurityContext{}
+		}
+		ct.SecurityContext.Privileged = ptr.Bool(n.execNodeSpec.Privileged)
+	}
+
+	if len(podSpec.Containers) != 1 {
+		log.Panicf("length of exec node containers is expected to be 1, actual %v", len(podSpec.Containers))
+	}
+	setContainerPrivileged(&podSpec.Containers[0])
+
+	for i := range podSpec.InitContainers {
+		setContainerPrivileged(&podSpec.InitContainers[i])
+	}
+
+	for _, sidecarConfig := range n.execNodeSpec.Sidecars {
+		sidecarSpec := corev1.Container{}
+		if err := yaml.Unmarshal([]byte(sidecarConfig), &sidecarSpec); err != nil {
+			return err
+		}
+		podSpec.Containers = append(podSpec.Containers, sidecarSpec)
+	}
+
+	return nil
 }
 
 func (n *execNode) doSync(ctx context.Context, dry bool) (ComponentStatus, error) {
@@ -88,31 +117,9 @@ func (n *execNode) doSync(ctx context.Context, dry bool) (ComponentStatus, error
 
 	if n.NeedSync() {
 		if !dry {
-			setContainerPrivileged := func(ct *corev1.Container) {
-				if ct.SecurityContext == nil {
-					ct.SecurityContext = &corev1.SecurityContext{}
-				}
-				ct.SecurityContext.Privileged = ptr.Bool(n.privileged)
-			}
-
-			statefulSet := n.server.buildStatefulSet()
-			containers := &statefulSet.Spec.Template.Spec.Containers
-			if len(*containers) != 1 {
-				log.Panicf("length of exec node containers is expected to be 1, actual %v", len(*containers))
-			}
-			setContainerPrivileged(&(*containers)[0])
-
-			initContainers := &statefulSet.Spec.Template.Spec.InitContainers
-			for i := range *initContainers {
-				setContainerPrivileged(&(*initContainers)[i])
-			}
-
-			for _, sidecarSpec := range n.sidecars {
-				sidecar := corev1.Container{}
-				if err := yaml.Unmarshal([]byte(sidecarSpec), &sidecar); err != nil {
-					return WaitingStatus(SyncStatusBlocked, "invalid sidecar"), err
-				}
-				*containers = append(*containers, sidecar)
+			err = n.doBuild()
+			if err != nil {
+				return WaitingStatus(SyncStatusBlocked, "cannot build exec node spec"), err
 			}
 			err = n.server.Sync(ctx)
 		}
