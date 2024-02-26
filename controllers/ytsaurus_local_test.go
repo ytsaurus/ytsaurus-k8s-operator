580a28f57c0404230663d762e24117aba78896ff
Kirill SIbirev <sibirev@nebius.com>
2024-02-26 17:11:41 +0100

    Example test with local kubeapi+etcd (#134)

---
+++ controllers/ytsaurus_local_test.go
@@ -0,0 +1,154 @@
+package controllers
+
+import (
+	"testing"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	ytv1 "github.com/ytsaurus/yt-k8s-operator/api/v1"
+	"github.com/ytsaurus/yt-k8s-operator/pkg/testutil"
+)
+
+const (
+	ytsaurusName      = "testsaurus"
+	testYtsaurusImage = "test-ytsaurus-image"
+	dndsNameOne       = "dn-1"
+)
+
+func TestYtsaurusFromScratch(t *testing.T) {
+	namespace := "ytsaurus-from-scratch"
+	h := testutil.NewTestHelper(t, namespace)
+	reconcilerSetup := func(mgr ctrl.Manager) error {
+		return (&YtsaurusReconciler{
+			Client:   mgr.GetClient(),
+			Scheme:   mgr.GetScheme(),
+			Recorder: mgr.GetEventRecorderFor("ytsaurus-controller"),
+		}).SetupWithManager(mgr)
+	}
+	h.Start(reconcilerSetup)
+	defer h.Stop()
+
+	ytsaurusResource := buildMinimalYtsaurus(namespace, ytsaurusName)
+	testutil.DeployObject(h, &ytsaurusResource)
+
+	// emulate master init job succeeded
+	testutil.MarkJobSucceeded(h, "yt-master-init-job-default")
+	testutil.MarkJobSucceeded(h, "yt-client-init-job-user")
+
+	for _, compName := range []string{
+		"discovery",
+		"master",
+		"http-proxy",
+	} {
+		testutil.FetchAndCheckConfigMapContainsEventually(
+			h,
+			"yt-"+compName+"-config",
+			"ytserver-"+compName+".yson",
+			"ms-0.masters."+namespace+".svc.cluster.local:9010",
+		)
+	}
+	testutil.FetchAndCheckConfigMapContainsEventually(
+		h,
+		"yt-data-node-"+dndsNameOne+"-config",
+		"ytserver-data-node.yson",
+		"ms-0.masters."+namespace+".svc.cluster.local:9010",
+	)
+
+	for _, stsName := range []string{
+		"ds",
+		"ms",
+		"hp",
+		"dnd-" + dndsNameOne,
+	} {
+		testutil.FetchEventually(
+			h,
+			stsName,
+			&appsv1.StatefulSet{},
+		)
+	}
+
+	testutil.FetchAndCheckEventually(
+		h,
+		"yt-client-secret",
+		&corev1.Secret{},
+		"secret with not empty token",
+		func(obj client.Object) bool {
+			secret := obj.(*corev1.Secret)
+			return len(secret.Data["YT_TOKEN"]) != 0
+		},
+	)
+
+	testutil.FetchAndCheckEventually(
+		h,
+		ytsaurusName,
+		&ytv1.Ytsaurus{},
+		"cluster state is running",
+		func(obj client.Object) bool {
+			state := obj.(*ytv1.Ytsaurus).Status.State
+			return state == ytv1.ClusterStateRunning
+		},
+	)
+}
+
+func buildMinimalYtsaurus(namespace, name string) ytv1.Ytsaurus {
+	return ytv1.Ytsaurus{
+		ObjectMeta: v1.ObjectMeta{Namespace: namespace, Name: name},
+		Spec: ytv1.YtsaurusSpec{
+			CommonSpec: ytv1.CommonSpec{
+				CoreImage:     testYtsaurusImage,
+				UseShortNames: true,
+			},
+			IsManaged:        true,
+			EnableFullUpdate: false,
+
+			Discovery: ytv1.DiscoverySpec{
+				InstanceSpec: ytv1.InstanceSpec{
+					InstanceCount: 3,
+				},
+			},
+			PrimaryMasters: ytv1.MastersSpec{
+				InstanceSpec: ytv1.InstanceSpec{
+					InstanceCount: 3,
+					Locations: []ytv1.LocationSpec{
+						{
+							LocationType: "MasterChangelogs",
+							Path:         "/yt/master-data/master-changelogs",
+						},
+						{
+							LocationType: "MasterSnapshots",
+							Path:         "/yt/master-data/master-snapshots",
+						},
+					},
+				},
+				MasterConnectionSpec: ytv1.MasterConnectionSpec{
+					CellTag: 1,
+				},
+			},
+			HTTPProxies: []ytv1.HTTPProxiesSpec{
+				{
+					InstanceSpec: ytv1.InstanceSpec{InstanceCount: 3},
+					ServiceType:  corev1.ServiceTypeNodePort,
+				},
+			},
+			DataNodes: []ytv1.DataNodesSpec{
+				{
+					InstanceSpec: ytv1.InstanceSpec{
+						InstanceCount: 5,
+						Locations: []ytv1.LocationSpec{
+							{
+								LocationType: "ChunkStore",
+								Path:         "/yt/node-data/chunk-store",
+							},
+						},
+					},
+					ClusterNodesSpec: ytv1.ClusterNodesSpec{},
+					Name:             dndsNameOne,
+				},
+			},
+		},
+	}
+}
---
+++ pkg/testutil/testhelper.go
@@ -0,0 +1,233 @@
+package testutil
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/go-logr/logr/testr"
+	"github.com/stretchr/testify/require"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	ytv1 "github.com/ytsaurus/yt-k8s-operator/api/v1"
+)
+
+type TestHelper struct {
+	t          *testing.T
+	ctx        context.Context
+	cancel     context.CancelFunc
+	k8sTestEnv *envtest.Environment
+	k8sClient  client.Client
+	cfg        *rest.Config
+	namespace  string
+}
+
+func NewTestHelper(t *testing.T, namespace string) *TestHelper {
+	if os.Getenv("KUBEBUILDER_ASSETS") == "" {
+		t.Fatal(
+			"KUBEBUILDER_ASSETS needed to be set for this test " +
+				"Something like KUBEBUILDER_ASSETS=`bin/setup-envtest use 1.24.2 -p path` would do." +
+				"Check Makefile for the details.",
+		)
+	}
+	k8sTestEnv := &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+		CRDInstallOptions: envtest.CRDInstallOptions{
+			MaxTime: 60 * time.Second,
+		},
+		ControlPlane: envtest.ControlPlane{},
+	}
+
+	testCtx, testCancel := context.WithCancel(context.Background())
+
+	return &TestHelper{
+		t:          t,
+		ctx:        testCtx,
+		cancel:     testCancel,
+		k8sTestEnv: k8sTestEnv,
+		namespace:  namespace,
+	}
+}
+
+func (h *TestHelper) Start(reconcilerSetup func(mgr ctrl.Manager) error) {
+	t := h.t
+	conf, err := h.k8sTestEnv.Start()
+	require.NoError(t, err)
+	require.NotNil(t, conf)
+	h.cfg = conf
+
+	err = ytv1.AddToScheme(scheme.Scheme)
+	require.NoError(t, err)
+
+	mgr, err := ctrl.NewManager(conf, ctrl.Options{
+		Scheme: scheme.Scheme,
+		// To get rid of macOS' accept incoming network connections popup
+		MetricsBindAddress:     "0",
+		HealthProbeBindAddress: "0",
+		Logger:                 testr.New(t),
+	})
+	require.NoError(t, err)
+
+	h.createNamespace()
+
+	err = reconcilerSetup(mgr)
+	require.NoError(t, err)
+
+	go func() {
+		err = mgr.Start(h.ctx)
+		require.NoError(t, err)
+	}()
+}
+
+func (h *TestHelper) Stop() {
+	// Should cancel ctx before Stop
+	// https://github.com/kubernetes-sigs/controller-runtime/issues/1571#issuecomment-945535598
+	h.cancel()
+	err := h.k8sTestEnv.Stop()
+	require.NoError(h.t, err)
+}
+
+func (h *TestHelper) GetK8sClient() client.Client {
+	if h.k8sClient == nil {
+		k8sCli, err := client.New(h.cfg, client.Options{Scheme: scheme.Scheme})
+		require.NoError(h.t, err)
+		h.k8sClient = k8sCli
+	}
+	return h.k8sClient
+}
+
+func (h *TestHelper) GetObjectKey(name string) client.ObjectKey {
+	return client.ObjectKey{Name: name, Namespace: h.namespace}
+}
+
+func (h *TestHelper) GetObjectMeta(name string) metav1.ObjectMeta {
+	return metav1.ObjectMeta{Name: name, Namespace: h.namespace}
+}
+
+func (h *TestHelper) createNamespace() {
+	c := h.GetK8sClient()
+	ns := corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: h.namespace,
+		},
+	}
+	err := c.Create(context.Background(), &ns)
+	require.NoError(h.t, err)
+}
+
+// helpers
+func GetObject(h *TestHelper, key string, emptyObject client.Object) {
+	k8sCli := h.GetK8sClient()
+	err := k8sCli.Get(context.Background(), h.GetObjectKey(key), emptyObject)
+	require.NoError(h.t, err)
+}
+func DeployObject(h *TestHelper, object client.Object) {
+	k8sCli := h.GetK8sClient()
+	err := k8sCli.Create(context.Background(), object)
+	require.NoError(h.t, err)
+}
+func UpdateObject(h *TestHelper, emptyObject, newObject client.Object) {
+	k8sCli := h.GetK8sClient()
+
+	//key := client.ObjectKey{Name: newObject.GetName(), Namespace: newObject.GetNamespace()}
+	GetObject(h, newObject.GetName(), emptyObject)
+
+	newObject.SetResourceVersion(emptyObject.GetResourceVersion())
+	err := k8sCli.Update(context.Background(), newObject)
+	require.NoError(h.t, err)
+}
+func UpdateObjectStatus(h *TestHelper, newObject client.Object) {
+	k8sCli := h.GetK8sClient()
+	err := k8sCli.Status().Update(context.Background(), newObject)
+	require.NoError(h.t, err)
+}
+
+func MarkJobSucceeded(h *TestHelper, key string) {
+	job := &batchv1.Job{}
+	FetchEventually(h, key, job)
+	job.Status.Succeeded = 1
+	UpdateObjectStatus(h, job)
+}
+
+const (
+	eventuallyWaitTime = 10 * time.Second
+	eventuallyTickTime = 500 * time.Millisecond
+)
+
+func FetchEventually(h *TestHelper, key string, obj client.Object) {
+	FetchAndCheckEventually(
+		h,
+		key,
+		obj,
+		"object successfully fetched",
+		func(obj client.Object) bool {
+			return true
+		})
+}
+func FetchAndCheckEventually(h *TestHelper, key string, obj client.Object, condDescription string, condition func(obj client.Object) bool) {
+	k8sCli := h.GetK8sClient()
+	Eventually(
+		h,
+		fmt.Sprintf("condition '%s' for resource: `%s`", condDescription, key),
+		func() bool {
+			err := k8sCli.Get(context.Background(), h.GetObjectKey(key), obj)
+			if err != nil {
+				if errors.IsNotFound(err) {
+					h.t.Logf("object %v not found.", key)
+					return false
+				}
+				require.NoError(h.t, err)
+			}
+			return condition(obj)
+		},
+	)
+}
+func Eventually(h *TestHelper, description string, condition func() bool) {
+	h.t.Log("start waiting " + description)
+	waitTime := eventuallyWaitTime
+	// Useful when debugging test.
+	waitTimeFromEnv := os.Getenv("TEST_EVENTUALLY_WAIT_TIME")
+	if waitTimeFromEnv != "" {
+		var err error
+		waitTime, err = time.ParseDuration(waitTimeFromEnv)
+		if err != nil {
+			h.t.Fatalf("failed to parse TEST_EVENTUALLY_WAIT_TIME=%s", waitTimeFromEnv)
+		}
+	}
+	require.Eventually(
+		h.t,
+		condition,
+		waitTime,
+		eventuallyTickTime,
+	)
+}
+
+func FetchAndCheckConfigMapContainsEventually(h *TestHelper, objectKey, cmKey, expectSubstr string) {
+	var cmData map[string]string
+	FetchAndCheckEventually(
+		h,
+		objectKey,
+		&corev1.ConfigMap{},
+		"config map exists",
+		func(obj client.Object) bool {
+			cmData = obj.(*corev1.ConfigMap).Data
+			return true
+		},
+	)
+	require.Contains(h.t, cmData, cmKey)
+	ysonContent := cmData[cmKey]
+	require.Contains(h.t, ysonContent, expectSubstr)
+
+}
