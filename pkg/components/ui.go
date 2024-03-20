954a82f496b8f27f6a1a5d47c4eb5b263d6d82e8
Nurmukhammad Kozubaev <nkozubaeff@gmail.com>
2024-03-20 16:03:10 +0300

    Add ALLOW_PASSWORD_AUTH to UI component

--- pkg/components/ui.go
+++ pkg/components/ui.go
@@ -146,9 +146,14 @@ func (u *UI) syncComponents(ctx context.Context) (err error) {
 	}
 
 	env := []corev1.EnvVar{
+		// Deprecated since v 17.0.0
 		{
 			Name:  "YT_AUTH_CLUSTER_ID",
 			Value: ytsaurusResource.Name,
+		},
+		{
+			Name:  "ALLOW_PASSWORD_AUTH",
+			Value: "1",
 		},
 		{
 			Name:  "APP_INSTALLATION",
