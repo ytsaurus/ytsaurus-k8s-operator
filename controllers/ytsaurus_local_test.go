2fb2bf0f823a6a75ed927e27cd8040d0ef2db91a
Kirill Sibirev <sibirev@nebius.com>
2024-03-21 16:41:38 +0100

    Testutil improvements

--- controllers/ytsaurus_local_test.go
+++ controllers/ytsaurus_local_test.go
@@ -36,10 +36,6 @@ func TestYtsaurusFromScratch(t *testing.T) {
 	ytsaurusResource := testutil.BuildMinimalYtsaurus(namespace, ytsaurusName)
 	testutil.DeployObject(h, &ytsaurusResource)
 
-	// emulate master init job succeeded
-	testutil.MarkJobSucceeded(h, "yt-master-init-job-default")
-	testutil.MarkJobSucceeded(h, "yt-client-init-job-user")
-
 	for _, compName := range []string{
 		"discovery",
 		"master",
