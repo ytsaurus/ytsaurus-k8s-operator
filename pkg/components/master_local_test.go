795296801bb7080b80f84c6bc19923e4eb5736d4
Kirill Sibirev <sibirev@nebius.com>
2024-03-22 17:19:14 +0100

    some reuse in tests

--- pkg/components/master_local_test.go
+++ pkg/components/master_local_test.go
@@ -2,21 +2,10 @@ package components
 
 import (
 	"context"
-	"fmt"
-	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/require"
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/tools/record"
-	ptr "k8s.io/utils/pointer"
-	ctrlrt "sigs.k8s.io/controller-runtime"
 
 	ytv1 "github.com/ytsaurus/yt-k8s-operator/api/v1"
 	apiProxy "github.com/ytsaurus/yt-k8s-operator/pkg/apiproxy"
-	"github.com/ytsaurus/yt-k8s-operator/pkg/testutil"
 	"github.com/ytsaurus/yt-k8s-operator/pkg/ytconfig"
 )
 
@@ -43,65 +32,15 @@ func (y *fakeYtsaurusForMaster) AreMasterSnapshotsBuilt(context.Context, []strin
 }
 
 func TestMasterFlow(t *testing.T) {
-	ctx := context.Background()
-	namespace := "master"
-	domain := "testdomain"
-
-	h := testutil.NewTestHelper(t, namespace, filepath.Join("..", "..", "config", "crd", "bases"))
-	h.Start(func(mgr ctrlrt.Manager) error { return nil })
-	defer h.Stop()
-
-	ytsaurusResource := testutil.BuildMinimalYtsaurus(namespace, ytsaurusName)
-	// Deploy of ytsaurus spec is required, so it could set valid owner references for child resources.
-	testutil.DeployObject(h, &ytsaurusResource)
-
-	scheme := runtime.NewScheme()
-	utilruntime.Must(ytv1.AddToScheme(scheme))
-	fakeRecorder := record.NewFakeRecorder(100)
-
-	ytsaurus := apiProxy.NewYtsaurus(&ytsaurusResource, h.GetK8sClient(), fakeRecorder, scheme)
-	cfgen := ytconfig.NewGenerator(&ytsaurusResource, domain)
-
-	// initial creation
-	master := NewMaster(cfgen, ytsaurus, &fakeYtsaurusForMaster{})
-	testutil.Eventually(h, "ms became ready", func() bool {
-		st, err := master.Status(ctx)
-		require.NoError(t, err)
-		if st.SyncStatus == SyncStatusReady {
-			return true
-		}
-		require.NoError(t, master.Sync(ctx))
-		return false
-	})
-
-	cmData := testutil.FetchConfigMapData(h, "yt-master-config", "ytserver-master.yson")
-	require.Contains(t, cmData, "ms-0.masters."+namespace+".svc."+domain+":9010")
-
-	testutil.FetchEventually(
-		h,
+	testComponentFlow(
+		t,
 		"ms",
-		&appsv1.StatefulSet{},
+		"master",
+		func(cfgen *ytconfig.Generator, ytsaurus *apiProxy.Ytsaurus) Component {
+			return NewMaster(cfgen, ytsaurus, &fakeYtsaurusForMaster{})
+		},
+		func(ytsaurus *ytv1.Ytsaurus, image *string) {
+			ytsaurus.Spec.PrimaryMasters.Image = image
+		},
 	)
-
-	// update + update #2 to be sure that first update doesn't end with wrong state
-	// TODO: 2 updates
-	for i := 1; i <= 1; i++ {
-		t.Logf("Update ms #%d", i)
-		newImage := ptr.String(fmt.Sprintf("new-image-%d", i))
-		ytsaurusResource.Spec.PrimaryMasters.Image = newImage
-
-		master = NewMaster(cfgen, ytsaurus, &fakeYtsaurusForMaster{})
-		testutil.Eventually(h, "ms became ready", func() bool {
-			st, err := master.Status(ctx)
-			require.NoError(t, err)
-			if st.SyncStatus == SyncStatusReady {
-				return true
-			}
-			require.NoError(t, master.Sync(ctx))
-			return false
-		})
-		sts := &appsv1.StatefulSet{}
-		testutil.GetObject(h, "ms", sts)
-		require.Equal(t, *newImage, sts.Spec.Template.Spec.Containers[0].Image)
-	}
 }
