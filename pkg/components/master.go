028033c8598c3d89b7478b596097d8e347ac75fe
Grigory Reznikov <gritukan@nebius.com>
2024-03-26 15:42:09 +0100

    Medium is called domestic medium now (#188)
    * Medium is called domestic medium now
    
    * wip

--- pkg/components/master.go
+++ pkg/components/master.go
@@ -170,7 +170,8 @@ func (m *Master) initMedia() string {
 		if err != nil {
 			panic(err)
 		}
-		commands = append(commands, fmt.Sprintf("/usr/bin/yt get //sys/media/%s/@name || /usr/bin/yt create medium --attr '%s'", medium.Name, string(attr)))
+		// COMPAT(gritukan): Remove "medium" after some time.
+		commands = append(commands, fmt.Sprintf("/usr/bin/yt get //sys/media/%s/@name || /usr/bin/yt create domestic_medium --attr '%s' || /usr/bin/yt create medium --attr '%s'", medium.Name, string(attr), string(attr)))
 	}
 	return strings.Join(commands, "\n")
 }
