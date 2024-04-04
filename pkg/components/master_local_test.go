1d72da22f284d4562575fab7e4eb92a2e59de165
Kirill Sibirev <l0kix2@gmail.com>
2024-04-04 10:46:36 +0200

    reuse some things in master local test

--- pkg/components/master_local_test.go
+++ pkg/components/master_local_test.go
@@ -3,21 +3,14 @@ package components
 import (
 	"context"
 	"fmt"
-	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/tools/record"
 	ptr "k8s.io/utils/pointer"
-	ctrlrt "sigs.k8s.io/controller-runtime"
 
 	ytv1 "github.com/ytsaurus/yt-k8s-operator/api/v1"
-	"github.com/ytsaurus/yt-k8s-operator/pkg/apiproxy"
 	"github.com/ytsaurus/yt-k8s-operator/pkg/testutil"
-	"github.com/ytsaurus/yt-k8s-operator/pkg/ytconfig"
 )
 
 type fakeYtsaurusForMaster struct {
@@ -43,31 +36,14 @@ func (y *fakeYtsaurusForMaster) AreMasterSnapshotsBuilt(context.Context, []strin
 }
 
 func TestMasterFlow(t *testing.T) {
+	ctx := context.Background()
 	shortName := "ms"
 	longName := "master"
-	setImage := func(ytsaurus *ytv1.Ytsaurus, image *string) {
-		ytsaurus.Spec.PrimaryMasters.Image = image
-	}
-
-	ctx := context.Background()
 	namespace := longName
 
-	h := testutil.NewTestHelper(t, namespace, filepath.Join("..", "..", "config", "crd", "bases"))
-	h.Start(func(mgr ctrlrt.Manager) error { return nil })
+	h, ytsaurus, cfgen := prepareTest(t, namespace)
 	defer h.Stop()
 
-	ytsaurusResource := testutil.BuildMinimalYtsaurus(namespace, ytsaurusName)
-	// Deploy of ytsaurus spec is required, so it could set valid owner references for child resources.
-	testutil.DeployObject(h, &ytsaurusResource)
-
-	scheme := runtime.NewScheme()
-	utilruntime.Must(ytv1.AddToScheme(scheme))
-	fakeRecorder := record.NewFakeRecorder(100)
-
-	ytsaurus := apiproxy.NewYtsaurus(&ytsaurusResource, h.GetK8sClient(), fakeRecorder, scheme)
-	cfgen := ytconfig.NewGenerator(&ytsaurusResource, domain)
-
-	// initial creation
 	component := NewMaster(cfgen, ytsaurus, &fakeYtsaurusForMaster{})
 	testutil.Eventually(h, shortName+" became ready", func() bool {
 		needBuild, err := component.NeedBuild(ctx)
@@ -89,24 +65,19 @@ func TestMasterFlow(t *testing.T) {
 		&appsv1.StatefulSet{},
 	)
 
+	setImage := func(ytsaurus *ytv1.Ytsaurus, image *string) {
+		ytsaurus.Spec.PrimaryMasters.Image = image
+	}
 	// update
 	// TODO: update2 to be sure that first update doesn't end with wrong state
 	// after fix jobs deletion
 	for i := 1; i <= 1; i++ {
 		t.Logf("Update %s #%d", shortName, i)
 		newImage := ptr.String(fmt.Sprintf("new-image-%d", i))
-		setImage(&ytsaurusResource, newImage)
+		setImage(ytsaurus.GetResource(), newImage)
 
 		component = NewMaster(cfgen, ytsaurus, &fakeYtsaurusForMaster{})
-		testutil.Eventually(h, shortName+" became ready", func() bool {
-			st, err := component.Status(ctx)
-			require.NoError(t, err)
-			if st.SyncStatus == SyncStatusReady {
-				return true
-			}
-			require.NoError(t, component.Sync(ctx))
-			return false
-		})
+		syncUntilReady(t, h, component)
 		sts := &appsv1.StatefulSet{}
 		testutil.GetObject(h, shortName, sts)
 		require.Equal(t, *newImage, sts.Spec.Template.Spec.Containers[0].Image)
