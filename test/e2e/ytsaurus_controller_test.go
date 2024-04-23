f78427705e529e2c7913cc7a7e1416fb9b584272
Kirill Sibirev <l0kix2@gmail.com>
2024-04-23 12:53:06 +0200

    Fix indentation

--- test/e2e/ytsaurus_controller_test.go
+++ test/e2e/ytsaurus_controller_test.go
@@ -263,8 +263,7 @@ var _ = Describe("Basic test for Ytsaurus controller", func() {
 			getSimpleUpdateScenario("test-major-update", ytv1.CoreImageNextVer),
 		)
 		It(
-			"Should be updated according to UpdateSelector=Everything",
-			func(ctx context.Context) {
+			"Should be updated according to UpdateSelector=Everything", func(ctx context.Context) {
 				namespace := "testslcteverything"
 
 				By("Creating a Ytsaurus resource")
@@ -307,8 +306,7 @@ var _ = Describe("Basic test for Ytsaurus controller", func() {
 			},
 		)
 		It(
-			"Should be updated according to UpdateSelector=TabletNodesOnly,ExecNodesOnly",
-			func(ctx context.Context) {
+			"Should be updated according to UpdateSelector=TabletNodesOnly,ExecNodesOnly", func(ctx context.Context) {
 				namespace := "testslctnodes"
 
 				By("Creating a Ytsaurus resource")
@@ -358,8 +356,7 @@ var _ = Describe("Basic test for Ytsaurus controller", func() {
 			},
 		)
 		It(
-			"Should be updated according to UpdateSelector=MasterOnly,StatelessOnly",
-			func(ctx context.Context) {
+			"Should be updated according to UpdateSelector=MasterOnly,StatelessOnly", func(ctx context.Context) {
 				namespace := "testslctother"
 
 				By("Creating a Ytsaurus resource")
