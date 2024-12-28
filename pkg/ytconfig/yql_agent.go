a12074b0f18e88b4bd4289c2c886566b78d72eac
Marina Pereskokova <mvpereskokova@gmail.com>
2024-12-28 16:54:20 +0300

    Configure yqla mrjob syslibs

--- pkg/ytconfig/yql_agent.go
+++ pkg/ytconfig/yql_agent.go
@@ -11,10 +11,15 @@ type ClusterMapping struct {
 	Default bool   `yson:"default"`
 }
 
+type MrJobSystemLibsWithMd5 struct {
+	File string `yson:"file"`
+}
+
 type GatewayConfig struct {
-	MRJobBinary    string           `yson:"mr_job_bin"`
-	UDFDirectory   string           `yson:"mr_job_udfs_dir"`
-	ClusterMapping []ClusterMapping `yson:"cluster_mapping"`
+	MRJobBinary            string                   `yson:"mr_job_bin"`
+	UDFDirectory           string                   `yson:"mr_job_udfs_dir"`
+	ClusterMapping         []ClusterMapping         `yson:"cluster_mapping"`
+	MrJobSystemLibsWithMd5 []MrJobSystemLibsWithMd5 `yson:"mr_job_system_libs_with_md5"`
 }
 
 type YQLAgent struct {
@@ -50,6 +55,14 @@ func getYQLAgentServerCarcass(spec *ytv1.YQLAgentSpec) (YQLAgentServer, error) {
 	c.User = "yql_agent"
 
 	c.YQLAgent.GatewayConfig.UDFDirectory = "/usr/lib/yql"
+	c.YQLAgent.GatewayConfig.MrJobSystemLibsWithMd5 = []MrJobSystemLibsWithMd5{
+		MrJobSystemLibsWithMd5{
+			File: "/usr/lib/yql/libiconv.so",
+		},
+		MrJobSystemLibsWithMd5{
+			File: "/usr/lib/yql/liblibidn-dynamic.so",
+		},
+	}
 	c.YQLAgent.GatewayConfig.MRJobBinary = "/usr/bin/mrjob"
 	c.YQLAgent.YqlPluginSharedLibrary = "/usr/lib/yql/libyqlplugin.so"
 
