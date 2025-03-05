f33f0ad60a2f1723a8c2fb7d3a593610e9e428c9
Kirill Sibirev <sibirev@nebius.com>
2025-03-05 15:21:05 +0100

    Switch to different update trigger in e2e (#456)
    * Switch to different update trigger in e2e
    
    * Switch to different update trigger in e2e

--- test/e2e/helpers_test.go
+++ test/e2e/helpers_test.go
@@ -7,7 +7,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/utils/ptr"
 	ctrlcli "sigs.k8s.io/controller-runtime/pkg/client"
+
+	ytv1 "github.com/ytsaurus/ytsaurus-k8s-operator/api/v1"
 )
 
 func getComponentPods(ctx context.Context, namespace string) map[string]corev1.Pod {
@@ -59,4 +62,18 @@ func getChangedPods(before, after map[string]corev1.Pod) changedObjects {
 		}
 	}
 	return ret
+}
+
+// updateSpecToTriggerAllComponentUpdate is a helper
+// that introduce spec change which should trigger change in all component static configs
+// and thus trigger all components update.
+func updateSpecToTriggerAllComponentUpdate(ytsaurus *ytv1.Ytsaurus) {
+	// ForceTCP is a field that is used in all components static configs
+	// so changing it should trigger all components update.
+	// Any value should be ok for test purposes.
+	if ytsaurus.Spec.ForceTCP == nil {
+		ytsaurus.Spec.ForceTCP = ptr.To(true)
+	} else {
+		ytsaurus.Spec.ForceTCP = ptr.To(!*ytsaurus.Spec.ForceTCP)
+	}
 }
