41fcba20a5300309a3672273cdff30e323dc7911
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2025-07-08 11:32:17 +0200

    Cleanup RPC service resource construction
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@tracto.ai>

--- pkg/resources/rpc_service.go
+++ pkg/resources/rpc_service.go
@@ -2,20 +2,17 @@ package resources
 
 import (
 	"github.com/ytsaurus/ytsaurus-k8s-operator/pkg/apiproxy"
-	"github.com/ytsaurus/ytsaurus-k8s-operator/pkg/consts"
 	labeller "github.com/ytsaurus/ytsaurus-k8s-operator/pkg/labeller"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 type RPCService struct {
 	BaseManagedResource[*corev1.Service]
 
-	port     *int32
-	nodePort *int32
+	Ports []corev1.ServicePort
 }
 
-func NewRPCService(name string, labeller *labeller.Labeller, apiProxy apiproxy.APIProxy) *RPCService {
+func NewRPCService(name string, ports []corev1.ServicePort, labeller *labeller.Labeller, apiProxy apiproxy.APIProxy) *RPCService {
 	return &RPCService{
 		BaseManagedResource: BaseManagedResource[*corev1.Service]{
 			proxy:     apiProxy,
@@ -24,36 +21,15 @@ func NewRPCService(name string, labeller *labeller.Labeller, apiProxy apiproxy.A
 			oldObject: &corev1.Service{},
 			newObject: &corev1.Service{},
 		},
+		Ports: ports,
 	}
-}
-
-func (s *RPCService) SetNodePort(port *int32) {
-	s.nodePort = port
-}
-
-func (s *RPCService) SetPort(port *int32) {
-	s.port = port
 }
 
 func (s *RPCService) Build() *corev1.Service {
-	var port int32 = consts.RPCProxyRPCPort
-	if s.port != nil {
-		port = *s.port
-	}
-	servicePort := corev1.ServicePort{
-		Name:       "rpc",
-		Port:       port,
-		TargetPort: intstr.IntOrString{IntVal: port},
-	}
-	if s.nodePort != nil {
-		servicePort.NodePort = *s.nodePort
-	}
-
 	s.newObject.ObjectMeta = s.labeller.GetObjectMeta(s.name)
 	s.newObject.Spec = corev1.ServiceSpec{
 		Selector: s.labeller.GetSelectorLabelMap(),
-		Ports:    []corev1.ServicePort{servicePort},
+		Ports:    s.Ports,
 	}
-
 	return s.newObject
 }
