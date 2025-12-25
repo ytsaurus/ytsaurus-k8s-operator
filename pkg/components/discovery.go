1ebc2f43436913fd8f472f8765994869da110a36
isbakurskii <isbakurskii@tracto.ai>
2025-12-25 22:10:22 +0100

    Added Discovery to the bulkUpdate mode

--- pkg/components/discovery.go
+++ pkg/components/discovery.go
@@ -2,6 +2,7 @@ package components
 
 import (
 	"context"
+	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
 
@@ -10,14 +11,16 @@ import (
 	"github.com/ytsaurus/ytsaurus-k8s-operator/pkg/consts"
 	"github.com/ytsaurus/ytsaurus-k8s-operator/pkg/resources"
 	"github.com/ytsaurus/ytsaurus-k8s-operator/pkg/ytconfig"
+	"go.ytsaurus.tech/yt/go/ypath"
 )
 
 type Discovery struct {
 	localServerComponent
-	cfgen *ytconfig.Generator
+	cfgen          *ytconfig.Generator
+	ytsaurusClient internalYtsaurusClient
 }
 
-func NewDiscovery(cfgen *ytconfig.Generator, ytsaurus *apiproxy.Ytsaurus) *Discovery {
+func NewDiscovery(cfgen *ytconfig.Generator, ytsaurus *apiproxy.Ytsaurus, yc internalYtsaurusClient) *Discovery {
 	l := cfgen.GetComponentLabeller(consts.DiscoveryType, "")
 	resource := ytsaurus.GetResource()
 
@@ -41,6 +44,7 @@ func NewDiscovery(cfgen *ytconfig.Generator, ytsaurus *apiproxy.Ytsaurus) *Disco
 	return &Discovery{
 		localServerComponent: newLocalServerComponent(l, ytsaurus, srv),
 		cfgen:                cfgen,
+		ytsaurusClient:       yc,
 	}
 }
 
@@ -56,8 +60,23 @@ func (d *Discovery) doSync(ctx context.Context, dry bool) (ComponentStatus, erro
 	}
 
 	if d.ytsaurus.GetClusterState() == ytv1.ClusterStateUpdating {
-		if status, err := handleUpdatingClusterState(ctx, d.ytsaurus, d, &d.localComponent, d.server, dry); status != nil {
-			return *status, err
+		if IsUpdatingComponent(d.ytsaurus, d) {
+			switch getComponentUpdateStrategy(d.ytsaurus, consts.DiscoveryType, d.GetShortName()) {
+			case ytv1.ComponentUpdateModeTypeOnDelete:
+				if status, err := handleOnDeleteUpdatingClusterState(ctx, d.ytsaurus, d, &d.localComponent, d.server, dry); status != nil {
+					return *status, err
+				}
+			default:
+				if status, err := handleBulkUpdatingClusterState(ctx, d.ytsaurus, d, &d.localComponent, d.server, dry); status != nil {
+					return *status, err
+				}
+			}
+
+			if d.ytsaurus.GetUpdateState() != ytv1.UpdateStateWaitingForPodsCreation {
+				return ComponentStatusReady(), err
+			}
+		} else {
+			return ComponentStatusReadyAfter("Not updating component"), nil
 		}
 	}
 
@@ -82,4 +101,40 @@ func (d *Discovery) Status(ctx context.Context) (ComponentStatus, error) {
 func (d *Discovery) Sync(ctx context.Context) error {
 	_, err := d.doSync(ctx, false)
 	return err
+}
+
+func (d *Discovery) UpdatePreCheck(ctx context.Context) ComponentStatus {
+	if d.ytsaurusClient == nil {
+		return ComponentStatusBlocked("YtsaurusClient component is not available")
+	}
+
+	ytClient := d.ytsaurusClient.GetYtClient()
+	if ytClient == nil {
+		return ComponentStatusBlocked("YT client is not available")
+	}
+
+	// Check that the number of instances in YT matches the expected instanceCount
+	expectedCount := int(d.ytsaurus.GetResource().Spec.Discovery.InstanceCount)
+	if err := IsInstanceCountEqualYTSpec(ctx, ytClient, consts.DiscoveryType, expectedCount); err != nil {
+		return ComponentStatusBlocked(err.Error())
+	}
+
+	var discoveryServers []string
+	cypressPath := consts.ComponentCypressPath(consts.DiscoveryType)
+
+	err := d.ytsaurusClient.GetYtClient().ListNode(ctx, ypath.Path(cypressPath), &discoveryServers, nil)
+	if err != nil {
+		return ComponentStatusBlocked(err.Error())
+	}
+
+	// Check that all discovery_servers are alive
+	for _, server := range discoveryServers {
+		orchidPath := ypath.Path(fmt.Sprintf("%s/%s/orchid", cypressPath, server))
+		var orchidData map[string]interface{}
+		if err := ytClient.GetNode(ctx, orchidPath, &orchidData, nil); err != nil {
+			return ComponentStatusBlocked(fmt.Sprintf("Failed to get discovery orchid data for %s: %v", server, err))
+		}
+	}
+
+	return ComponentStatusReady()
 }
