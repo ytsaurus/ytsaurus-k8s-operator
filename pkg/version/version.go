6798b6819cbfa05db118777fb806ef1e94899143
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2026-01-28 13:32:22 +0100

    Switch devel version to 0.0.0-devel and different library
    We already have github.com/Masterminds/semver as dependencies.
    It is much better documented. It use use more widely used syntax and
    can handle pre-release version.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@tracto.ai>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 
 	"runtime/debug"
+
+	"github.com/Masterminds/semver/v3"
 )
 
 // version provided by ldflags at compile-time
 var version string
 
+const (
+	DevelVersion = "0.0.0-devel"
+)
+
 func GetVersion() string {
 	if version == "" {
 		if i, ok := debug.ReadBuildInfo(); ok {
 			version = i.Main.Version
+			if version == "(devel)" {
+				version = DevelVersion
+			}
 		}
 	}
 	return version
@@ -20,4 +29,29 @@ func GetVersion() string {
 
 func PrintVersion() {
 	fmt.Println(GetVersion())
+}
+
+type Version = semver.Version
+
+func ParseVersion(version string) (*Version, error) {
+	return semver.StrictNewVersion(version)
+}
+
+type Constraints = semver.Constraints
+
+func ParseConstraints(constraints string) (*Constraints, error) {
+	cs, err := semver.NewConstraint(constraints)
+	if err != nil {
+		return nil, err
+	}
+	cs.IncludePrerelease = true
+	return cs, nil
+}
+
+func GetParsedVersion() *Version {
+	ver, err := ParseVersion(GetVersion())
+	if err != nil {
+		ver = semver.New(0, 0, 0, "malformed", "")
+	}
+	return ver
 }
