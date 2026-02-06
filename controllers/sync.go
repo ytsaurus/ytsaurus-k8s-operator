869cecd4bb67f9a1b15ae3517b894e7c1c91b337
isbakurskii <isbakurskii@tracto.ai>
2026-02-06 15:21:56 +0100

    Implemented ImageHeater component

--- controllers/sync.go
+++ controllers/sync.go
@@ -70,6 +70,15 @@ func chooseUpdatingComponents(selectors []ytv1.ComponentUpdateSelector, needUpda
 	return canUpdate, cannotUpdate
 }
 
+func hasComponentType(components []ytv1.Component, componentType consts.ComponentType) bool {
+	for _, component := range components {
+		if component.Type == componentType {
+			return true
+		}
+	}
+	return false
+}
+
 func hasEverythingSelector(selectors []ytv1.ComponentUpdateSelector) bool {
 	for _, selector := range selectors {
 		if selector.Class == consts.ComponentClassEverything {
