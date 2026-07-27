1c239676330a675422fe9631ed400a1cdeae744e
Konstantin Khlebnikov <khlebnikov@nebius.com>
2026-07-27 14:33:24 +0200

    Fix admin user creation
    The two-phase creation could fail by commit timeout, but leave object with
    "@life_stage" != "creation_committed", which could become committed later.
    
    As a result admin user could exists but not as a superuser.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@nebius.com>

--- pkg/components/user.go
+++ pkg/components/user.go
@@ -21,6 +21,11 @@ func createUserCommand(userName, password, token string, isSuperuser bool) []str
 		fmt.Sprintf("/usr/bin/yt create user --attributes '{name=\"%s\"}' --ignore-existing", userName),
 	}
 
+	if isSuperuser {
+		result = append(result, fmt.Sprintf("/usr/bin/yt add-member %s superusers || true", userName))
+		result = append(result, fmt.Sprintf(`while [ "$(/usr/bin/yt check-permission --format json %s administer /)" != '{"action":"allow"}' ] ; do sleep 1 ; done`, userName))
+	}
+
 	if password != "" {
 		passwordHash := sha256String(password)
 		result = append(result, fmt.Sprintf("/usr/bin/yt execute set_user_password '{user=%s;new_password_sha256=\"%s\"}'", userName, passwordHash))
@@ -30,11 +35,6 @@ func createUserCommand(userName, password, token string, isSuperuser bool) []str
 		tokenHash := sha256String(token)
 		result = append(result, fmt.Sprintf("/usr/bin/yt create map_node '//sys/cypress_tokens/%s' --ignore-existing", tokenHash))
 		result = append(result, fmt.Sprintf("/usr/bin/yt set '//sys/cypress_tokens/%s/@user' '%s'", tokenHash, userName))
-	}
-
-	if isSuperuser {
-		result = append(result, fmt.Sprintf("/usr/bin/yt add-member %s superusers || true", userName))
-		result = append(result, fmt.Sprintf(`while [ "$(/usr/bin/yt check-permission --format json %s administer /)" != '{"action":"allow"}' ] ; do sleep 1 ; done`, userName))
 	}
 
 	return result
